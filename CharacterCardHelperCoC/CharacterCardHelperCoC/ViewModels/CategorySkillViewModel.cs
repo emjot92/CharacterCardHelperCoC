7e9c280a32d4696845835fdcdfc6b43e78377cd5
Tomasz Patyk <tomasz.patyk@aptiv.com>
2020-03-13 16:07:55 +0100

    Add skill view models

---
+++ CharacterCardHelperCoC/CharacterCardHelperCoC/ViewModels/CategorySkillViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CoCHelpers.Classes;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Reactive;
+
+namespace CharacterCardHelperCoC.ViewModels
+{
+    public class CategorySkillViewModel : SkillViewModel
+    {
+        private readonly CategorySkill _categorySkill;
+
+        public CategorySkillViewModel(CategorySkill skill) : base(skill)
+        {
+            _categorySkill = skill;
+            Skills = new ReadOnlyObservableCollection<Skill>(_categorySkill.Skills);
+            AddSkill = ReactiveCommand.Create<Skill>(s =>
+            {
+                _categorySkill.Skills.Add(s);
+            });
+        }
+
+        public ReadOnlyObservableCollection<Skill> Skills { get; }
+
+        public ReactiveCommand<Skill, Unit> AddSkill { get; }
+    }
+}
---
+++ CharacterCardHelperCoC/CharacterCardHelperCoC/ViewModels/SkillViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CoCHelpers.Classes;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace CharacterCardHelperCoC.ViewModels
+{
+    public class SkillViewModel : ReactiveObject
+    {
+        private readonly Skill _skill;
+
+        public SkillViewModel(Skill skill)
+        {
+            _skill = skill;
+        }
+
+        public string Name => _skill.Name;
+
+        public int StartingValue => _skill.StartingValue;
+
+        [Reactive]
+        public int Value { get => _skill.Value; set => _skill.Value = value; }
+
+        public SkillCategory Category => _skill.Category;
+
+        public bool IsModern => _skill.IsModern;
+    }
+}
